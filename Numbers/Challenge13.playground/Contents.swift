71a7fb62cdfb174755458cd824e32ca18e15e6d8
Luis Fernando Salas Gave <lsalagav@emeal.nttdata.com>
2023-01-13 15:00:05 -0500

    Challenge13

---
+++ Numbers/Challenge13.playground/Contents.swift
@@ -0,0 +1,28 @@
+/*
+ Challenge 13: Fizz Buzz
+ Write a function that counts from 1 through 100, and prints “Fizz” if the counter is evenly divisible by 3, “Buzz” if it’s evenly divisible by 5, “Fizz Buzz” if it’s even divisible by three and five, or the counter number for all other cases.
+ Sample input and output
+ • 1 should print “1”
+ • 2 should print “2”
+ • 3 should print “Fizz”
+ • 4 should print “4”
+ • 5 should print “Buzz”
+ • 15 should print “Fizz Buzz”
+ */
+
+func challenge13() {
+    
+    for i in 1...100 {
+        if i % 15 == 0 {
+            print("Fizz Buzz")
+        } else if i % 5 == 0 {
+            print("Buzz")
+        } else if i % 3 == 0 {
+            print("Fizz")
+        } else {
+            print(i)
+        }
+    }
+}
+
+challenge13()
--- Strings/Challenge11.playground/Contents.swift
+++ Strings/Challenge11.playground/Contents.swift
@@ -1,3 +1,35 @@
 import UIKit
 
-var greeting = "Hello, playground"
+/*
+ Challenge 11: Run-length encoding
+ Write a function that accepts a string as input, then returns how often each letter is repeated in a single run, taking case into account.
+ Tip: This approach is used in a simple lossless compression technique called run-length encoding.
+ Sample input and output
+ • The string “aabbcc” should return “a2b2c2”.
+ • The strings “aaabaaabaaa” should return “a3b1a3b1a3”
+ • The string “aaAAaa” should return “a2A2a2”
+ */
+
+func challenge11(_ input: String) {
+  let inputArray = Array(input)
+  var firstLetter = inputArray[1]
+  var letterCount = 0
+  var result = ""
+  
+        
+    
+    for (index, char) in inputArray.enumerated() {
+        if char == inputArray[index]{
+            letterCount += 1
+        }
+    }
+    
+    print(letterCount)
+  
+}
+
+let input = "aabbcc"
+challenge11(input)
+//print(result)
+
+
